7feed7a0f7bb5ab3d6b127ed20762d461c2748d6
Vitali Filipchuk <vitali.filipchuk@gmail.com>
2021-12-06 22:52:39 +0200

    start creating routes

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="header">
+        <div class="container">
+            <div class="header__inner">
+                <router-link to="/">Головна</router-link>
+                <router-link to="/login">Увійти</router-link>
+                <router-link to="/registration">Реєстрація</router-link>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+
+}
+</script>
+
+<style>
+
+</style>
