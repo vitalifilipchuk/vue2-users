ac293b24dff6661c63776a2eb92e8dceace60b1b
Vitali Filipchuk <vitali.filipchuk@gmail.com>
2021-12-15 01:30:59 +0200

    fix some bugs

--- src/views/MyAccount.vue
+++ src/views/MyAccount.vue
@@ -2,20 +2,27 @@
   <div class="account">
     <div class="container">
       <h1>Особистий кабінет</h1>
-      <Logout @logout-user="$emit('logout-user')" :isLoggedIn="isLoggedIn" v-if="isLoggedIn" />
+      <Button 
+          class="logout-btn" 
+          :text="'Вийти'" 
+          v-if="isLoggedIn" 
+          @click.native="$emit('logoutUser')"   
+      />
     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Button from '../components/Button'
 
 export default {
   name: 'Account',
+  components: {
+    Button
+  },
   props: {
     isLoggedIn: Boolean
-  },
-  components: {
   }
 }
 </script>
